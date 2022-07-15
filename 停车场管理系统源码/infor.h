3ca2b2af31b7d30f608dab37beef42c2d91ae603
signword <56613574+signword@users.noreply.github.com>
2022-07-15 15:21:21 +0800

    Create infor.h

---
+++ 停车场管理系统源码/infor.h
@@ -0,0 +1,15 @@
+#ifndef __INFOR__
+#define __INFOR__
+#include<stdio.h>
+#include<time.h>
+#include<string.h>
+
+
+extern vehicle ve[1001];
+extern int n;
+extern int sum;
+extern double fee[5];
+extern char name[21];
+
+
+#endif
