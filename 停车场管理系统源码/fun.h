c57a9f2af6ee5b3654aba67623f316590a93f9a8
signword <56613574+signword@users.noreply.github.com>
2022-07-15 15:20:53 +0800

    Create fun.h

---
+++ 停车场管理系统源码/fun.h
@@ -0,0 +1,22 @@
+#ifndef __FUN__
+#define __FUN__
+#include<stdio.h>
+#include<time.h>
+#include<string.h>
+
+void output(int i);
+void menu();
+void display(); 
+void cost(int i);
+int confirm(int i);
+void interface(int i);
+void settings();
+int compare(char a[],int b);
+void record(int i);
+int liceninqu(char a[],char b[]);
+void inquire(int t);
+void modelstats();
+void timestats();
+//void licenstats();
+
+#endif
